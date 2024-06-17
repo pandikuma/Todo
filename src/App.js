33f9977ec3a8740d478f389a6d4b655d38d6a7e8
pandikuma <pandeeswaran12022002@gmail.com>
2024-06-17 09:57:21 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios"
+
 
 function App() {
+
+
+  const [enterdvalue,setevalue] = useState("")
+  const [fruit,setfruit] = useState([])
+
+  useEffect(function(){
+    axios.get("http://localhost:5000/fruitlist").then(function(data){
+      setfruit(data.data)
+    })
+  },[])
+
+
+
+
+  function handlevalue(evt)
+  {
+    setevalue(evt.target.value)
+  }
+
+  function add()
+  {
+    axios.post("http://localhost:5000/addfruit",{newfruit:enterdvalue})
+
+
+    setfruit([...fruit,{name:enterdvalue}])
+    setevalue("")
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={handlevalue}></input>
+
+      <button onClick={add}>Add</button>
+
+      {fruit.map(function(item,index){
+        return <h1 key={index}>{item.name}</h1>
+      })}
     </div>
   );
 }
